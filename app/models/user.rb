ed23c7f276b0a72222929c9a1c4306e9fb3bbbb7
Vania Januar <vjanuarcs@gmail.com>
2015-05-06 21:05:24 +1000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy  # arranges for the dependent microposts to be destroyed when the user itself is destroyed.
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
 	before_save { self.email = email.downcase }
   attr_accessor :remember_token
   validates :name,  presence: true, length: { maximum: 50 }
@@ -9,6 +20,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
+
+
 
   # returns the hash digest of the given string.
   def User.digest(string)
@@ -38,4 +51,27 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,20 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
+
+
+users = User.order(:created_at).take(6)    # select the first six users (the '.order' call ensures we select the first 6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
